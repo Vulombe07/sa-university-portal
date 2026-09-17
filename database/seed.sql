95f2efe7772f831bc507a86eab42ebe13ac060bd
ostekek <tobennamoloi@gmail.com>
2026-09-17 22:01:17 +0200

    Adding subjects
    Insert multiple subjects into the subjects table for demo purposes.

---
+++ database/seed.sql
@@ -0,0 +1,16 @@
+--Creating subjects to populate table before demo
+INSERT INTO subjects (name)
+VALUES
+    ('Mathematics'),
+    ('Mathematical Literacy'),
+    ('English Home Language'),
+    ('Afrikaans First Additional Language'),
+    ('isiZulu First Additional Language'),
+    ('Life Sciences'),
+    ('Physical Sciences'),
+    ('Computer Applications Technology'),
+    ('Information Technology'),
+    ('Accounting'),
+    ('Business Studies'),
+    ('History'),
+    ('Geography');

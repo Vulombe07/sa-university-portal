e291a88f5142c61c2bc96118c3ad7ee6a02c949a
ostekek <tobennamoloi@gmail.com>
2026-09-17 22:20:46 +0200

    Adding Unis

--- database/seed.sql
+++ database/seed.sql
@@ -14,3 +14,48 @@ VALUES
     ('Business Studies'),
     ('History'),
     ('Geography');
+
+--Inserting unis into table
+INSERT INTO universities (
+    name,
+    short_name,
+    province,
+    website,
+    aps_method
+)
+VALUES
+    (
+        'University of the Witwatersrand',
+        'WITS',
+        'Gauteng',
+        'https://www.wits.ac.za/',
+        'WITS'
+    ),
+    (
+        'University of Cape Town',
+        'UCT',
+        'Western Cape',
+        'https://uct.ac.za/',
+        'UCT'
+    ),
+    (
+        'University of Pretoria',
+        'UP',
+        'Gauteng',
+        'https://www.up.ac.za/',
+        'UP'
+    ),
+    (
+        'Stellenbosch University',
+        'SU',
+        'Western Cape',
+        'https://www.su.ac.za/en',
+        'STELLENBOSCH'
+    ),
+    (
+        'University of the Western Cape',
+        'UWC',
+        'Western Cape',
+        'https://www.uwc.ac.za/',
+        'UWC'
+    );
